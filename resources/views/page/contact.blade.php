f70748af148962740ef35421df1dfe0620bd48ae
EmilFateev <98778582+EmilFateev@users.noreply.github.com>
2022-03-10 09:47:14 +0400

    Contact route and map

--- resources/views/page/contact.blade.php
+++ resources/views/page/contact.blade.php
@@ -78,12 +78,12 @@
                 </div>
             </div>
             <div class="uk-map uk-margin-top">
-                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.1320402645842!2d-71.43840028204887!3d42.700924847595665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3ba675a27778f%3A0xae3aaad0339c335!2z0KTQtdC30LDQvdGCINCb0LXQudC9INCc0L7Qu9C7!5e0!3m2!1sru!2sru!4v1631357483152!5m2!1sru!2sru" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1466.0565635800162!2d-71.43832351786189!3d42.70132533709657!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e3bb7684b32d09%3A0xe13c920570e107d5!2sEinsteiners%20LLC!5e0!3m2!1sen!2sru!4v1646891172211!5m2!1sen!2sru" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
             </div>
             <div>
                 <div class="uk-flex uk-flex-center uk-margin-top">
                     <div class="uk-button">
-                        <a href="https://g.page/PheasantLaneMall?share" target="_blank">build a route</a>
+                        <a href="https://www.google.com/maps/place/Einsteiners+LLC/@42.7013253,-71.4383235,18z/data=!3m1!5s0x89e3ba67467d1d05:0x7edf0a3a6093e300!4m5!3m4!1s0x89e3bb7684b32d09:0xe13c920570e107d5!8m2!3d42.7013214!4d-71.4373311?hl=en" target="_blank">Route to Einsteiners</a>
                     </div>
                 </div>
             </div>
